a5b3008c8cf1876acafbae0c985de4f94d11e441
Ash Pook <ashleighpook@gmail.com>
2015-09-24 16:34:37 +0100

    `EitherT` does exist in cats, it's called `XorT`.

--- source/zenith.context/src/main/scala/Context.scala
+++ source/zenith.context/src/main/scala/Context.scala
@@ -33,61 +33,34 @@ import java.io.PrintStream
 /**
  * WTF! For some reason Cats doesn't define `WriterT`
  */
-final case class WriterT[F[_], L, V] (run: F[(L, V)]) {
-  def written (implicit f: Functor[F]): F[L] = f.map (run)(_._1)
-  def value (implicit f: Functor[F]): F[V] = f.map (run)(_._2)
-  def map[Z](fn: V => Z)(implicit f: Functor[F]): WriterT[F, L, Z] = WriterT {
+final case class WriterT[T[_], L, V] (run: T[(L, V)]) {
+  def written (implicit functorT: Functor[T]): T[L] = functorT.map (run)(_._1)
+  def value (implicit functorT: Functor[T]): T[V] = functorT.map (run)(_._2)
+  def map[Z](fn: V => Z)(implicit f: Functor[T]): WriterT[T, L, Z] = WriterT {
     f.map (run) { z =>
       (z._1, fn (z._2))
     }
   }
-  //Equivalent to `join (map (fa)(f))`.
-  def flatMap[V2](f: V => WriterT[F, L, V2])(implicit M: Monad[F], S: Semigroup[L]): WriterT[F, L, V2] = WriterT {
-    M.flatMap (run) { lv =>
-      M.map (f (lv._2).run) { lv2 =>
-        (S.combine (lv._1, lv2._1), lv2._2)
+  def flatMap[U](f: V => WriterT[T, L, U])(implicit monadT: Monad[T], semigroupL: Semigroup[L]): WriterT[T, L, U] = WriterT {
+    monadT.flatMap (run) { lv =>
+      monadT.map (f (lv._2).run) { lv2 =>
+        (semigroupL.combine (lv._1, lv2._1), lv2._2)
       }
     }
   }
 }
 object WriterT {
-  def putT[T[_], L, V] (vt: T[V])(l: L)(implicit f: Functor[T]): WriterT[T, L, V] = WriterT (f.map (vt)(v => (l, v)))
-  def put[T[_], L, V] (v: V)(l: L)(implicit f: Functor[T], a: Applicative[T]): WriterT[T, L, V] = WriterT.putT[T, L, V](a.pure (v))(l)
-  def tell[T[_], L] (l: L)(implicit f: Functor[T], a: Applicative[T]): WriterT[T, L, Unit] = WriterT.put[T, L, Unit](())(l)
-  def value[T[_], L, V] (v: V)(implicit f: Functor[T],  a: Applicative[T], m: Monoid[L]): WriterT[T, L, V] = WriterT.put[T, L, V](v)(m.empty)
-  def valueT[T[_], L, V] (vt: T[V])(implicit f: Functor[T], m: Monoid[L]): WriterT[T, L, V] = WriterT.putT[T, L, V](vt)(m.empty)
+  def putT[T[_], L, V] (vt: T[V])(l: L)(implicit functorT: Functor[T]): WriterT[T, L, V] = WriterT (functorT.map (vt)(v => (l, v)))
+  def put[T[_], L, V] (v: V)(l: L)(implicit functorT: Functor[T], applicativeT: Applicative[T]): WriterT[T, L, V] = WriterT.putT[T, L, V](applicativeT.pure (v))(l)
+  def tell[T[_], L] (l: L)(implicit functorT: Functor[T], applicativeT: Applicative[T]): WriterT[T, L, Unit] = WriterT.put[T, L, Unit](())(l)
+  def value[T[_], L, V] (v: V)(implicit functorT: Functor[T], applicativeT: Applicative[T], monoidL: Monoid[L]): WriterT[T, L, V] = WriterT.put[T, L, V](v)(monoidL.empty)
+  def valueT[T[_], L, V] (vt: T[V])(implicit functorT: Functor[T], monoidL: Monoid[L]): WriterT[T, L, V] = WriterT.putT[T, L, V](vt)(monoidL.empty)
 
-  implicit def writerTMonad[T[_], L] (implicit monadT: Monad[T], monoid: Monoid[L]) = new Monad[({type WT[α] = WriterT[T, L, α]})#WT] {
+  implicit def writerTMonad[T[_], L] (implicit monadT: Monad[T], monoidL: Monoid[L]) = new Monad[({type WT[α] = WriterT[T, L, α]})#WT] {
     override def pure[A] (a: A): ({type WT[α] = WriterT[T, L, α]})#WT[A] = WriterT.value[T, L, A](a)
     override def flatMap[A, B] (fa: ({type WT[α] = WriterT[T, L, α]})#WT[A])(f: A => ({type WT[α] = WriterT[T, L, α]})#WT[B]): WriterT[T, L, B] = fa.flatMap (a => f (a))
     override def ap[A, B] (fa: ({type WT[α] = WriterT[T, L, α]})#WT[A])(ff: ({type WT[α] = WriterT[T, L, α]})#WT[A => B]): ({type WT[α] = WriterT[T, L, α]})#WT[B] = fa.flatMap (a => ff.map (f => f (a)))
   }
-}
-
-/**
- * OMFG!! For some reason Cats doesn't define `EitherT`
- */
-final case class EitherT[F[_], A, B] (run: F[A Either B]) {
-  def isLeft (implicit f: Functor[F]): F[Boolean] = f.map (run)(_.isLeft)
-  def isRight (implicit f: Functor[F]):F[Boolean] = f.map (run)(_.isRight)
-
-  def map[B2](fn: B => B2)(implicit f: Functor[F]): EitherT[F, A, B2] =
-    EitherT (f.map [A Either B, A Either B2](run) {
-      case Left (l) => Left[A, B2] (l)
-      case Right (r) => Right[A, B2] (fn (r))
-    })
-  def flatMap[B2](fn: B => EitherT[F, A, B2])(implicit m: Monad[F]) =
-    EitherT (m.flatMap [A Either B, A Either B2](run) {
-      case Left (l) => m.pure (Left[A, B2] (l))
-      case Right (r) => fn (r).run
-    })
-}
-
-object EitherT {
-  private def makeLeft[A, B]: A => A Either B = Left (_)
-  private def makeRight[A, B]: B => A Either B = Right (_)
-  def left[F[_], A, B] (a: F[A])(implicit f: Functor[F]): EitherT[F, A, B] = EitherT[F, A, B] (f.map (a)(makeLeft))
-  def right[F[_], A, B] (b: F[B])(implicit f: Functor[F]): EitherT[F, A, B] = EitherT[F, A, B] (f.map (b)(makeRight))
 }
 
 object Extensions extends PrintStreamExtensions with FutureExtensions
@@ -173,7 +146,7 @@ object Context {
   }
 
   type WF[$] = WriterT[Future, LoggingContext, $]
-  type EWT[$] = EitherT[WF, Throwable, $]
+  type EWT[$] = XorT[WF, Throwable, $]
   type CONTEXT[$] = EWT[$]
 
   def scalaFutureMonad (implicit ec: ExecutionContext): Monad[Future] = new Monad[Future] {
@@ -185,8 +158,8 @@ object Context {
   def printAndClear[T] (out: PrintStream, ec: ExecutionContext, onCrash: T)(c: CONTEXT[T]): CONTEXT[T] = {
     implicit val monadFuture: Monad[Future] = scalaFutureMonad (ec)
     import zenith.context.Extensions._
-    val ctxT = c.run.written
-    val vT = c.run.value.mapAll[T] {
+    val ctxT = c.toEither.written
+    val vT = c.toEither.value.mapAll[T] {
       case Success (s) => s match {
         case Right (v) => out.println ("Task in context completed successfully."); v
         case Left (ex) =>
@@ -213,46 +186,50 @@ object Context {
     }(ec)
 
     // clear all exceptions and logs
-    EitherT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](f))
+    XorT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](f))
   }
 
   def context (ec: ExecutionContext): zenith.Context[CONTEXT] = new zenith.Context[CONTEXT] {
     implicit val monadFuture: Monad[Future] = scalaFutureMonad (ec)
 
     /** Logger */
-    override def log (channel: => Option[String], level: => zenith.Logger.Level, message: => String): CONTEXT[Unit] =
-      Try { LoggingContext.log (channel, level, message) } match {
-      case Success (s) => EitherT.right[WF, Throwable, Unit](WriterT.put[Future, LoggingContext, Unit](())(s))
-      case Failure (f) => EitherT.left[WF, Throwable, Unit] (WriterT.value[Future, LoggingContext, Throwable](f))
+    override def log (channel: => Option[String], level: => zenith.Logger.Level, message: => String): CONTEXT[Unit] = {
+      println (s"$channel $level $message")
+      Try {
+        LoggingContext.log (channel, level, message)
+      } match {
+        case Success (s) => XorT.right[WF, Throwable, Unit](WriterT.put[Future, LoggingContext, Unit](())(s))
+        case Failure (f) => XorT.left[WF, Throwable, Unit](WriterT.value[Future, LoggingContext, Throwable](f))
+      }
     }
 
     /** Async */
     override def liftScalaFuture[T] (expression: => Future[T]): CONTEXT[T] = Try (expression) match {
-      case Success (s) => EitherT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](s))
-      case Failure (f) => EitherT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
+      case Success (s) => XorT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](s))
+      case Failure (f) => XorT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
     }
 
     override def future[T] (expression: => T): CONTEXT[T] = Try (expression) match {
-      case Success (s) => EitherT.right[WF, Throwable, T](WriterT.value[Future, LoggingContext, T](s))
-      case Failure (f) => EitherT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
+      case Success (s) => XorT.right[WF, Throwable, T](WriterT.value[Future, LoggingContext, T](s))
+      case Failure (f) => XorT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
     }
 
     override def success[T] (expression: => T): CONTEXT[T] = Try (expression) match {
-      case Success (s) => EitherT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](Future.successful (s)))
-      case Failure (f) => EitherT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
+      case Success (s) => XorT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](Future.successful (s)))
+      case Failure (f) => XorT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
     }
 
     override def failure[T] (expression: => Throwable): CONTEXT[T] = Try (expression) match {
-      case Success (s) => EitherT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](s))
-      case Failure (f) => EitherT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
+      case Success (s) => XorT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](s))
+      case Failure (f) => XorT.left[WF, Throwable, T] (WriterT.value[Future, LoggingContext, Throwable](f))
     }
 
-    override def onComplete[T, X](v: CONTEXT[T], f: Try[T] => X): Unit = v.run.value.map { case Left (l) => throw l; case Right (r) => r }(ec).onComplete (f)(ec)
+    override def onComplete[T, X](v: CONTEXT[T], f: Try[T] => X): Unit = v.toEither.value.map { case Left (l) => throw l; case Right (r) => r }(ec).onComplete (f)(ec)
     override def promise[T] (): Async.Promise[CONTEXT, T] = new Async.Promise[CONTEXT, T] {
       private val p = Promise[T] ()
       def success (x: T): Unit = p.success (x)
       def failure (x: Throwable): Unit = p.failure (x)
-      def future: CONTEXT[T] = EitherT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](p.future))
+      def future: CONTEXT[T] = XorT.right[WF, Throwable, T](WriterT.valueT[Future, LoggingContext, T](p.future))
     }
 
     /** Monad */
